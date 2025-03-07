071322b2bd2bb3b28d34c82615620bbc7ff360ca
Barazal <willi.abarnett@gmail.com>
2025-03-07 09:54:59 -0500

    Updates configuration of the tailwind file.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,26 +3,28 @@ import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
 import animate from "tailwindcss-animate";
 
-export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        accent: "var(--accent)",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", "sans-serif"],
-        mono: ["var(--font-geist-mono)", "monospace"],
-      },
-    },
-  },
-  plugins: [forms, typography, animate],
-}
+const config = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./lib/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				accent: "var(--accent)",
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			fontFamily: {
+				sans: ["var(--font-geist-sans)", "sans-serif"],
+				mono: ["var(--font-geist-mono)", "monospace"],
+			},
+		},
+	},
+	plugins: [forms, typography, animate],
+};
+
+export default config;
